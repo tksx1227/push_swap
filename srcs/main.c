8c63bf172fe197b1dd18e83a6c54e260cf536902
tomori <tmrtkm1227tksx@gmail.com>
2022-03-07 23:54:14 +0900

    [add] template of main.c

---
+++ srcs/main.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ttomori <ttomori@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/28 23:42:12 by ttomori           #+#    #+#             */
+/*   Updated: 2022/03/07 23:53:44 by ttomori          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+		return (1);
+	if (/* Check arguments */)
+	{
+		ft_dprintf(2, "Error\n");
+		return (1);
+	}
+	else
+	{
+		/* Create and sort two stacks */
+	}
+	return (0);
+}

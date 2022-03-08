53d3fbc7c5deddd5579a6d6d804efb782d8eae56
tomori <tmrtkm1227tksx@gmail.com>
2022-03-09 00:27:39 +0900

    [add] Types of operator and stack

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: ttomori <ttomori@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 23:53:50 by ttomori           #+#    #+#             */
-/*   Updated: 2022/02/28 23:54:56 by ttomori          ###   ########.fr       */
+/*   Updated: 2022/03/09 00:02:27 by ttomori          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,31 @@
 # define PUSH_SWAP_H
 
 # include "ft_dprintf.h"
+
+typedef enum e_operator
+{
+	SA,
+	SB,
+	PA,
+	PB,
+	RA,
+	RB,
+	RR,
+	RRA,
+	RRB,
+	RRR
+}	t_operator;
+
+typedef struct s_stack
+{
+	size_t	size;
+	t_list	*elements;
+}	t_stack;
+
+typedef struct s_container
+{
+	t_stack	*stack1;
+	t_stack	*stack2;
+}	t_container;
 
 #endif

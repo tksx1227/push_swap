0ca3730ac3550b9bf8e156b98d3aa2a71136a255
tomori <tmrtkm1227tksx@gmail.com>
2022-03-12 17:08:33 +0900

    [update] Check if it is within the int range

--- srcs/utils/check_args.c
+++ srcs/utils/check_args.c
@@ -6,18 +6,18 @@
 /*   By: ttomori <ttomori@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/12 14:42:08 by ttomori           #+#    #+#             */
-/*   Updated: 2022/03/12 16:06:29 by ttomori          ###   ########.fr       */
+/*   Updated: 2022/03/12 17:05:18 by ttomori          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static t_status	is_digit_all(size_t size, char **arr);
+static t_status	is_digit_all(int size, char **arr);
 static t_status	is_integer(char	*s);
 
-static t_status	is_digit_all(size_t size, char **arr)
+static t_status	is_digit_all(int size, char **arr)
 {
-	size_t	i;
+	int	i;
 
 	i = 0;
 	while (i < size)
@@ -31,16 +31,27 @@ static t_status	is_digit_all(size_t size, char **arr)
 
 static t_status	is_integer(char	*s)
 {
-	size_t	i;
+	int			i;
+	int			sign;
+	long long	n;
 
 	i = 0;
-	if (s[i] == '+' || s[i] == '-')
+	sign = 1;
+	if ((s[i] == '+' || s[i] == '-') && s[i + 1] != '\0')
+	{
 		i++;
+		sign = -1;
+	}
 	if (s[i] == '0' && s[i + 1] != '\0')
 		return (FAIL);
+	n = 0;
 	while (s[i] != '\0')
 	{
-		if (!ft_isdigit(s[i]))
+		if (ft_isdigit(s[i]))
+			n = n * 10 + sign * (s[i] - '0');
+		else
+			return (FAIL);
+		if (n < (long long)INT_MIN || (long long)INT_MAX < n)
 			return (FAIL);
 		i++;
 	}

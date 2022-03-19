b3e7a73c3bd187a9134110a73e6be012583d602b
tomori <tmrtkm1227tksx@gmail.com>
2022-03-19 12:59:01 +0900

    [update] Remove free_container and add free_stack

--- srcs/utils/free_stack.c
+++ srcs/utils/free_stack.c
@@ -1,28 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   free_container.c                                   :+:      :+:    :+:   */
+/*   free_stack.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ttomori <ttomori@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/13 01:22:59 by ttomori           #+#    #+#             */
-/*   Updated: 2022/03/13 14:31:59 by ttomori          ###   ########.fr       */
+/*   Updated: 2022/03/19 12:18:03 by ttomori          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static void	free_stack(t_stack *stack);
-
-void	free_container(t_container *container)
+void	free_stack(t_stack **stack)
 {
-	free_stack(container->stack1);
-	free_stack(container->stack2);
-	free(container);
-}
-
-static void	free_stack(t_stack *stack)
-{
-	ft_lstclear(&(stack->elements), free);
-	free(stack);
+	if (stack != NULL)
+	{
+		free((*stack)->elems);
+		free(*stack);
+		*stack = NULL;
+	}
 }

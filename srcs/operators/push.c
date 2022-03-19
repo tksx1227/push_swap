71758ccb33a0dfb6be4edac7059dde946c969b61
tomori <tmrtkm1227tksx@gmail.com>
2022-03-19 19:21:34 +0900

    [update] Free stacks when push is failed

--- srcs/operators/push.c
+++ srcs/operators/push.c
@@ -6,32 +6,43 @@
 /*   By: ttomori <ttomori@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/01 01:20:28 by ttomori           #+#    #+#             */
-/*   Updated: 2022/03/19 12:56:35 by ttomori          ###   ########.fr       */
+/*   Updated: 2022/03/19 19:20:57 by ttomori          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static void	add_one_elem(t_stack *stack, int n);
-static void	drop_one_elem(t_stack *stack);
+static t_stats	add_one_elem(t_stack *stack, int n);
+static t_stats	drop_one_elem(t_stack *stack);
 
 void	push_stack(t_stack *from_stack, t_stack *to_stack)
 {
+	t_status	status;
+
 	if (0 < from_stack->size)
 	{
-		add_one_elem(to_stack, from_stack->elems[0]);
-		drop_one_elem(from_stack);
+		status = add_one_elem(to_stack, from_stack->elems[0]);
+		if (status == SUCCESS)
+		{
+			status = drop_one_elem(from_stack);
+		}
+		if (status == FAIL)
+		{
+			free_stack(&from_stack);
+			free_stack(&to_stack);
+			exit(1);
+		}
 	}
 }
 
-static void	add_one_elem(t_stack *stack, int n)
+static t_status	add_one_elem(t_stack *stack, int n)
 {
 	size_t	i;
 	int		*new_elems;
 
 	new_elems = (int *)malloc(sizeof(int) * (stack->size + 1));
 	if (new_elems == NULL)
-		exit(2);
+		return (FAIL);
 	i = 0;
 	new_elems[i] = n;
 	while (i < stack->size)
@@ -42,16 +53,17 @@ static void	add_one_elem(t_stack *stack, int n)
 	free(stack->elems);
 	stack->elems = new_elems;
 	stack->size++;
+	return (SUCCESS);
 }
 
-static void	drop_one_elem(t_stack *stack)
+static t_status	drop_one_elem(t_stack *stack)
 {
 	size_t	i;
 	int		*new_elems;
 
 	new_elems = (int *)malloc(sizeof(int) * (stack->size - 1));
 	if (new_elems == NULL)
-		exit(2);
+		return (FAIL);
 	i = 0;
 	while (i < stack->size - 1)
 	{
@@ -61,4 +73,5 @@ static void	drop_one_elem(t_stack *stack)
 	free(stack->elems);
 	stack->elems = new_elems;
 	stack->size--;
+	return (SUCCESS);
 }

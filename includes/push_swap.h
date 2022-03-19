5aeda40b4b9cb69e52045a1cd9b3a6413a7ee5ae
tomori <tmrtkm1227tksx@gmail.com>
2022-03-19 13:04:11 +0900

    [update] Remove t_container and modified t_stack

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: ttomori <ttomori@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 23:53:50 by ttomori           #+#    #+#             */
-/*   Updated: 2022/03/18 10:14:34 by ttomori          ###   ########.fr       */
+/*   Updated: 2022/03/19 13:00:09 by ttomori          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,37 +18,23 @@
 typedef struct s_stack
 {
 	size_t	size;
-	t_list	*elements;
+	int		*elems;
 }	t_stack;
-
-typedef struct s_container
-{
-	t_stack	*stack1;
-	t_stack	*stack2;
-}	t_container;
 
 /* Utils */
 bool		is_valid_args(int size, char **args);
 bool		is_sorted_stack(t_stack *stack);
-t_container	*get_new_container(int size, char **arr);
-void		free_container(t_container *container);
-void		show_two_stacks(t_container *container);
+t_stack		*get_new_stack(int size, char **arr);
+void		free_stack(t_stack **stack);
+void		show_stack(t_stack *stack, char *name);
 
 /* Operators */
-// sa, sb, ss
-void		swap_a(t_container *container);
-void		swap_b(t_container *container);
-void		swap_ab(t_container *container);
-// pa, pb
-void		push_a(t_container *container);
-void		push_b(t_container *container);
-// ra, rb, rr
-void		rotate_a(t_container *container);
-void		rotate_b(t_container *container);
-void		rotate_ab(t_container *container);
-// rra, rrb, rrr
-void		rrotate_a(t_container *container);
-void		rrotate_b(t_container *container);
-void		rrotate_ab(t_container *container);
+void		swap_one_stack(t_stack *stack);
+void		swap_two_stacks(t_stack *stack1, t_stack *stack2);
+void		push_stack(t_stack *from_stack, t_stack *to_stack);
+void		rotate_one_stack(t_stack *stack);
+void		rotate_two_stacks(t_stack *stack1, t_stack *stack2);
+void		rrotate_one_stack(t_stack *stack);
+void		rrotate_two_stacks(t_stack *stack1, t_stack *stack2);
 
 #endif

11e92dbef610f3d39a9fd732d396b631a14dc7c0
tomori <tmrtkm1227tksx@gmail.com>
2022-03-19 13:03:48 +0900

    [update] Support new stack structure

--- srcs/operators/rotate.c
+++ srcs/operators/rotate.c
@@ -6,41 +6,32 @@
 /*   By: ttomori <ttomori@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/01 01:34:58 by ttomori           #+#    #+#             */
-/*   Updated: 2022/03/11 15:32:32 by ttomori          ###   ########.fr       */
+/*   Updated: 2022/03/19 12:04:35 by ttomori          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static void	rotate_base(t_stack *stack);
-
-void	rotate_a(t_container *container)
+void	rotate_one_stack(t_stack *stack)
 {
-	rotate_base(container->stack1);
-}
-
-void	rotate_b(t_container *container)
-{
-	rotate_base(container->stack2);
-}
-
-void	rotate_ab(t_container *container)
-{
-	rotate_a(container);
-	rotate_b(container);
-}
-
-static void	rotate_base(t_stack *stack)
-{
-	t_list	*head;
+	size_t	i;
+	int		first_num;
 
 	if (2 <= stack->size)
 	{
-		head = stack->elements;
-		stack->elements = stack->elements->next;
-		stack->elements->prev = NULL;
-		head->prev = NULL;
-		head->next = NULL;
-		ft_lstadd_back(&(stack->elements), head);
+		i = 0;
+		first_num = stack->elems[i];
+		while (i < stack->size - 1)
+		{
+			stack->elems[i] = stack->elems[i + 1];
+			i++;
+		}
+		stack->elems[i] = first_num;
 	}
+}
+
+void	rotate_two_stacks(t_stack *stack1, t_stack *stack2)
+{
+	rotate_one_stack(stack1);
+	rotate_one_stack(stack2);
 }
--- srcs/operators/swap.c
+++ srcs/operators/swap.c
@@ -6,38 +6,26 @@
 /*   By: ttomori <ttomori@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/01 00:55:34 by ttomori           #+#    #+#             */
-/*   Updated: 2022/03/11 15:14:07 by ttomori          ###   ########.fr       */
+/*   Updated: 2022/03/19 11:32:07 by ttomori          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static void	swap_base(t_stack *stack);
-
-void	swap_a(t_container *container)
+void	swap_one_stack(t_stack *stack)
 {
-	swap_base(container->stack1);
-}
-
-void	swap_b(t_container *container)
-{
-	swap_base(container->stack2);
-}
-
-void	swap_ab(t_container *container)
-{
-	swap_a(container);
-	swap_b(container);
-}
-
-static void	swap_base(t_stack *stack)
-{
-	void	*tmp;
+	int	tmp;
 
 	if (2 <= stack->size)
 	{
-		tmp = stack->elements->content;
-		stack->elements->content = stack->elements->next->content;
-		stack->elements->next->content = tmp;
+		tmp = stack->elems[0];
+		stack->elems[0] = stack->elems[1];
+		stack->elems[1] = tmp;
 	}
+}
+
+void	swap_two_stacks(t_stack *stack1, t_stack *stack2)
+{
+	swap_one_stack(stack1);
+	swap_one_stack(stack2);
 }

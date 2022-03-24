418605dda36973e09e5386c986f17c4a7dcc7203
tomori <tmrtkm1227tksx@gmail.com>
2022-03-24 09:26:48 +0900

    [fix] Fixed typo

--- srcs/checker/utils/do_operation.c
+++ srcs/checker/utils/do_operation.c
@@ -6,7 +6,7 @@
 /*   By: ttomori <ttomori@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/19 19:23:14 by ttomori           #+#    #+#             */
-/*   Updated: 2022/03/24 08:33:59 by ttomori          ###   ########.fr       */
+/*   Updated: 2022/03/24 09:26:32 by ttomori          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ t_status	do_operation(t_stack *stack1, t_stack *stack2, char *operator)
 		rrotate_and_set_operator(stack1, NULL);
 	else if (ft_strcmp(operator, "rrb\n") == 0)
 		rrotate_and_set_operator(stack2, NULL);
-	else if (ft_strcmp(operator, "rra\n") == 0)
+	else if (ft_strcmp(operator, "rrr\n") == 0)
 		rrotate_double_and_set_operator(stack1, stack2, NULL);
 	else
 		return (FAIL);

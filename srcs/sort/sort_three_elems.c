841bb388a487ccbd0b71f5e29a4f22917112cb5d
tomori <tmrtkm1227tksx@gmail.com>
2022-03-25 11:07:29 +0900

    [refactor] Logic simplification

--- srcs/sort/sort_three_elems.c
+++ srcs/sort/sort_three_elems.c
@@ -6,51 +6,39 @@
 /*   By: ttomori <ttomori@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 09:59:26 by ttomori           #+#    #+#             */
-/*   Updated: 2022/03/23 23:44:35 by ttomori          ###   ########.fr       */
+/*   Updated: 2022/03/25 11:06:40 by ttomori          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static void	sort_by_pattern(t_stack *stack, int pattern_no, t_list **operators);
+static size_t	get_max_idx(int *arr, size_t size);
 
 void	sort_three_elems(t_stack *stack, t_list **operators)
 {
-	int	first;
-	int	second;
-	int	third;
+	size_t	max_idx;
 
-	first = stack->elems[0];
-	second = stack->elems[1];
-	third = stack->elems[2];
-	if (second < first && second < third && first < third)
-		sort_by_pattern(stack, 1, operators);
-	else if (second < first && second < third && third < first)
-		sort_by_pattern(stack, 2, operators);
-	else if (first < second && third < second && third < first)
-		sort_by_pattern(stack, 3, operators);
-	else if (first < second && third < second && first < third)
-		sort_by_pattern(stack, 4, operators);
-	else if (second < first && third < second)
-		sort_by_pattern(stack, 5, operators);
+	max_idx = get_max_idx(stack->elems, stack->size);
+	if (max_idx == 0)
+		rotate_and_set_operator(stack, operators);
+	else if (max_idx == 1)
+		rrotate_and_set_operator(stack, operators);
+	if (stack->elems[1] < stack->elems[0])
+		swap_and_set_operator(stack, operators);
 }
 
-static void	sort_by_pattern(t_stack *stack, int pattern_no, t_list **operators)
+static size_t	get_max_idx(int *arr, size_t size)
 {
-	if (pattern_no == 1)
-		swap_and_set_operator(stack, operators);
-	else if (pattern_no == 2)
-		rotate_and_set_operator(stack, operators);
-	else if (pattern_no == 3)
-		rrotate_and_set_operator(stack, operators);
-	else if (pattern_no == 4)
+	size_t	i;
+	size_t	max_idx;
+
+	i = 0;
+	max_idx = 0;
+	while (i < size)
 	{
-		swap_and_set_operator(stack, operators);
-		rotate_and_set_operator(stack, operators);
+		if (arr[max_idx] < arr[i])
+			max_idx = i;
+		i++;
 	}
-	else if (pattern_no == 5)
-	{
-		swap_and_set_operator(stack, operators);
-		rrotate_and_set_operator(stack, operators);
-	}
+	return (max_idx);
 }
